655d31bdda7334665865b0c7463571827e8d5567
Emanuel Rukavina <git@erukavina.com>
2025-03-18 09:12:17 +0100

    Add utm params

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/blog">
+            to="/blog?utm_source=manurukavina.com">
             Read my ramblings
           </Link>
         </div>
